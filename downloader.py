57c5c2b9b03e2f7b20554153b098c70e3401960a
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-07-19 22:58:04 +0200

    Added function to download a webpage

--- downloader.py
+++ downloader.py
@@ -1,4 +1,11 @@
-import requests
+from urllib.request import urlopen, Request
+
+def get_page(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+    request = Request(url, headers=headers)
+    response = urlopen(request)
+    webContent = response.read()
+    print(webContent.decode("utf-8"))
 
 # Read the names of the mushrooms from a file
 with open('mushroom-list.txt', 'r') as file:
@@ -13,3 +20,6 @@ for name in names:
     words = name.split(' ')
     scientific_names.append((words[0], words[1]))
 
+# To get an specific mushroom: 
+# get_page('http://www.mushroom.world/show?n=Agaricus-arvensis')
+# http://www.mushroom.world/data/fungi/
